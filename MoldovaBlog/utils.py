25bf60633e13561e13112e5e548719f631fd677b
shunlog <artiombalan331@gmail.com>
2024-01-15 22:09:13 +0200

    minor fixes

--- MoldovaBlog/utils.py
+++ MoldovaBlog/utils.py
@@ -4,24 +4,25 @@ import datetime
 from django.conf import settings
 from django.core.mail import send_mail
 
+
+def create_verification_token(username, email):
+    '''Returns a string - the verification token (JWT).
+    The token should contain username and email.'''
+    payload = {
+        'username': str(username),
+        'email': str(email),
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=2)
+        }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+
 def send_verification_email(username, email):
-    '''Send an email verification link to email.'''
-
-    def create_verification_token():
-        '''Returns a string - the verification token (JWT).
-        The token should contain username and email.'''
-        payload = {
-            'username': str(username),
-            'email': str(email),
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=2)
-            }
-        return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-    token = create_verification_token()
+    '''Send verification link to email.'''
+    token = create_verification_token(username, email)
     url = reverse("email_verify", args=(token,))
     send_mail(
-        'Email Verification',
-        f'Click the link to verify your email: {url}',
+        'MoldovaBlog email verification',
+        f'Visit the link to verify your email: {url}',
         settings.EMAIL_HOST_USER,
         [email],
         fail_silently=False,
@@ -30,7 +31,8 @@ def send_verification_email(username, email):
 
 def validate_verification_token(token):
     '''Validates the email verification token.
-    Returns a tuple (username, email) if it's valid, or raises an exception otherwise.'''
+    Returns a tuple (username, email) if it's valid,
+    or raises an exception otherwise.'''
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
         return payload['username'], payload['email']
