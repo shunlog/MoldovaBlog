280d7a65fa6b100f8699dc53063b152f8f4f65a1
denis <dprodan289@gmail.com>
2024-01-15 19:27:38 +0200

    utils.py done

--- MoldovaBlog/settings.py
+++ MoldovaBlog/settings.py
@@ -144,3 +144,11 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'visitmoldova2023@gmail.com'
+EMAIL_HOST_PASSWORD = 'xime ulub ewub fgqd'
+EMAIL_USE_TLS = True
--- MoldovaBlog/utils.py
+++ MoldovaBlog/utils.py
@@ -1,5 +1,8 @@
 from django.urls import reverse
-
+import jwt
+import datetime
+from django.conf import settings
+from django.core.mail import send_mail
 
 def send_verification_email(username, email):
     '''Send an email verification link to email.'''
@@ -7,17 +10,26 @@ def send_verification_email(username, email):
     def create_verification_token():
         '''Returns a string - the verification token (JWT).
         The token should contain username and email.'''
-        pass
-
+        payload = {'username': str(username), 'email': email, "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=120),}
+        return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
     token = create_verification_token()
     url = reverse("email_verify", args=(token,))
-
-    pass
+    send_mail(
+        'Email Verification',
+        f'Click the link to verify your email: {url}',
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,
+    )
 
 
 def validate_verification_token(token):
     '''Validates the email verification token.
     Returns a tuple (username, email) if it's valid, or raises an exception otherwise.'''
-
-    raise ValueError
-    pass
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        return payload['username'], payload['email']
+    except jwt.exceptions.ExpiredSignatureError:
+        raise ValueError('Token has expired')
+    except (jwt.exceptions.InvalidSignatureError, jwt.exceptions.DecodeError):
+        raise ValueError('Invalid token')
