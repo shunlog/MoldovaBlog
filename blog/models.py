3d27b59bcaa09f0b7543094eea820091fd6939e5
shunlog <artiombalan331@gmail.com>
2024-01-17 23:15:29 +0200

    show dates on post detail

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -10,6 +13,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_time_passed(self):
+        return time_passed_string(self.pub_date)
 
 
 class Comment(models.Model):
@@ -24,3 +30,28 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[str(self.post.pk)])
+
+    def get_time_passed(self):
+        return time_passed_string(self.pub_date)
+
+
+def time_passed_string(t):
+    """Calculate a '3 hours ago' type string from a python datetime. """
+    units = {
+        'years': lambda diff: diff.days * 365,
+        'months': lambda diff: diff.days * 30,
+        'days': lambda diff: diff.days,
+        'hours': lambda diff: diff.seconds / 3600,
+        'minutes': lambda diff: diff.seconds % 3600 / 60,
+    }
+
+    now = timezone.now()
+    diff = now - t
+
+    for unit in units:
+        dur = int(units[unit](diff))
+        if dur > 0:
+            # De-pluralize if duration is 1 ('1 day' vs '2 days')
+            unit = unit[:-dur] if dur == 1 else unit
+            return f'{dur} {unit} ago'
+    return 'just now'
