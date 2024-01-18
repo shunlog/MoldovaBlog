813132f0db91c31e44a4b4c7fb068aa16cc51145
shunlog <artiombalan331@gmail.com>
2024-01-18 13:57:01 +0200

    fixup! show dates on post detail

--- blog/utils.py
+++ blog/utils.py
@@ -1,4 +1,5 @@
 import datetime
+from math import floor
 
 from django.utils import timezone
 
@@ -6,8 +7,8 @@ from django.utils import timezone
 def time_passed_string(t):
     """Calculate a '3 hours ago' type string from a python datetime. """
     units = {
-        'years': lambda diff: diff.days * 365,
-        'months': lambda diff: diff.days * 30,
+        'years': lambda diff: diff.days / 365,
+        'months': lambda diff: diff.days / 30,
         'days': lambda diff: diff.days,
         'hours': lambda diff: diff.seconds / 3600,
         'minutes': lambda diff: diff.seconds % 3600 / 60,
@@ -17,9 +18,10 @@ def time_passed_string(t):
     diff = now - t
 
     for unit in units:
-        dur = int(units[unit](diff))
-        if dur > 0:
-            # De-pluralize if duration is 1 ('1 day' vs '2 days')
-            unit = unit[:-dur] if dur == 1 else unit
-            return f'{dur} {unit} ago'
+        dur = floor(units[unit](diff))
+        if dur < 1:
+            continue
+        # De-pluralize if duration is 1 ('1 day' vs '2 days')
+        unit = unit[:-dur] if dur == 1 else unit
+        return f'{dur} {unit} ago'
     return 'just now'
