420b76b9bcbdd95764743033c0e96875ca68719c
shunlog <artiombalan331@gmail.com>
2024-01-25 02:00:15 +0200

    open it to the world!

--- MoldovaBlog/urls.py
+++ MoldovaBlog/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("blog/", include("blog.urls")),
+    path("", include("blog.urls")),
     path("admin/", admin.site.urls),
 
     path("accounts/verify/done", views.email_sent, name="email_sent"),
