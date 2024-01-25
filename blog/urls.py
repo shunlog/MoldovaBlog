69e7ca99988361e9c0dfe5a719ca9317c5136bf6
shunlog <artiombalan331@gmail.com>
2024-01-26 01:56:42 +0200

    nicer homepage

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.HomePageView.as_view(), name="index"),
+    path("about/", views.about, name="about"),
     path("posts/", views.PostListView.as_view(), name="post_list"),
     path("<int:pk>", views.PostDetailView.as_view(), name="post_detail"),
     path("user/edit", views.ProfileUpdateView.as_view(), name="profile_update"),
--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,29 @@ from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView
-from django.views.generic import DetailView, ListView, UpdateView
+from django.views.generic import DetailView, ListView, UpdateView, TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count
 from django import forms
 
 from .models import Post, Comment, Profile
 from .forms import CommentForm
-from . import utils
 
 
-def index(request):
-    return render(request, "blog/index.html")
+class HomePageView(TemplateView):
+    template_name = "blog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["latest_posts"] = Post.objects.order_by("-pub_date")[:4]
+        context["popular_posts"] = Post.objects.annotate(num_comm=Count("comment")).order_by("-num_comm")[:4]
+
+        return context
+
+
+def about(request):
+    return render(request, "blog/about.html")
 
 
 class UserDetailView(DetailView):
